a9ae7492d9738119f0eff06da028515b833f66fd
Christianne Cesar <43531282+bbchriscesar@users.noreply.github.com>
2024-06-21 00:22:50 +0800

    Update test1.spec.js

--- tests/week-5/test1.spec.js
+++ tests/week-5/test1.spec.js
@@ -1,13 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
-test.skip('test1 - example test in the same file', async ({ page }) => {
+test('test1 - example test in the same file', async ({ page }) => {
     await page.goto('https://conduit.bondaracademy.com/');
     await expect(page.locator('app-layout-header')).toContainText('Home');
     await expect(page.locator('app-layout-header')).toContainText('Sign in');
     await expect(page.locator('app-layout-header')).toContainText('Sign up');
 });
 
-test.skip('test2 - another test in the same file', async ({ page }) => {
+test('test2 - another test in the same file', async ({ page }) => {
     await page.goto('https://conduit.bondaracademy.com/');
     await expect(page.locator('app-home-page')).toContainText('Your Feed');
     await expect(page.locator('app-home-page')).toContainText('Global Feed');
